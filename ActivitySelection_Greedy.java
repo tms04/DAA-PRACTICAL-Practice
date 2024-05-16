bb3463d97bc3e9132da1d1ac859c15efcdbdbbfb
tms04 <138638288+tms04@users.noreply.github.com>
2024-05-16 08:38:48 +0000

    completed activity selection problem

--- ActivitySelection_Greedy.java
+++ ActivitySelection_Greedy.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 
 public class ActivitySelection_Greedy {
+    private static int[] ActicitySelection(int[] start, int[] finish, int num) {
+        int X[]=new int[num];
+        X[0]=1;
+        int currentTime=finish[0];
+        for(int i=1;i<num;i++){
+            X[i]=0;
+            if(currentTime<=start[i]){
+                X[i]=1;
+                currentTime=finish[i];
+            }
+        }
+        return X;
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the number of activities");
@@ -15,6 +28,31 @@ public class ActivitySelection_Greedy {
             finish[i]=sc.nextInt();
             index[i]=i+1;
         }
-        
+        for(int i=0;i<num;i++){
+            for(int j=i;j<num;j++){
+                if(finish[i]>finish[j]){
+                    int t=finish[i];
+                    finish[i]=finish[j];
+                    finish[j]=t;
+
+                    t=start[i];
+                    start[i]=start[j];
+                    start[j]=t;
+
+                    t=index[i];
+                    index[i]=index[j];
+                    index[j]=t;
+                }
+            }
+        }
+        int X[]=ActicitySelection(start,finish,num);
+        for(int i=0;i<num;i++){
+            if(X[i]==1){
+                System.out.println("Activity number "+index[i]+" is selected");
+            }else{
+                System.out.println("Activity number "+index[i]+" is not selected");
+            }
+            
+        }
     }
 }
